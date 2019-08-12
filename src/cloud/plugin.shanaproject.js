2144fbb5e0800f25af9a59954e73e27d780b9d0d
ephellon <minkcbos@gmail.com>
2019-08-12 06:29:29 -0500

    Added Kitsu. Updated layouts

--- src/cloud/plugin.shanaproject.js
+++ src/cloud/plugin.shanaproject.js
@@ -2,20 +2,22 @@
 // Aurthor(s) - @ephellon (2018)
 let plugin = {
     "url": "*://*.shanaproject.com/series/\\d+",
+
     "init": () => {
         let title = document.queryBy('.overview i, #header_big .header_info_block')
-            .first.textContent.trim(),
+                .first.textContent.trim(),
             year = +document.queryBy('#header_big .header_info_block + *')
-            .first.textContent.trim()
-            .replace(/[^]*(\d{4})[^]*/m, '$1'),
+                .first.textContent.trim()
+                .replace(/[^]*(\d{4})[^]*/m, '$1'),
             image = document.queryBy('#header_big .header_display_box')
-            .first.style['background-image'].trim()
-            .replace(/url\((.+)\)/i, '$1');
+                .first.style['background-image'].trim()
+                .replace(/url\((.+)\)/i, '$1');
+
         return {
             type: 'show',
             title,
             year,
             image
         };
-    }
+    },
 };
