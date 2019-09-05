f5b704b1a4199eba5603c480229f2445f8033c4d
ephellon <minkcbos@gmail.com>
2019-09-04 19:23:07 -0600

    Updated download scripts (deterred use of blobs). Updated packages.

--- src/download/consistent.js
+++ src/download/consistent.js
@@ -11,8 +11,13 @@ check = document.body.onload = event => {
     let video = document.querySelector('video');
 
     if(video && video.src) {
+        let { src } = video;
+
+        if(/^blob:/i.test(src))
+            throw '<blob> URL detected. Unable to reform file.';
+
         try {
-            top.postMessage({ href: video.src, tail: 'mp4', type: 'SEND_VIDEO_LINK', from: 'consistent' }, '*');
+            top.postMessage({ href: src, tail: 'MP4', type: 'SEND_VIDEO_LINK', from: 'consistent' }, '*');
         } catch(error) {
             terminal.error('Failed to post message:', error);
         }
